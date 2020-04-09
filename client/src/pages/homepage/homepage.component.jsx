8a70f8da3a9da7d8ffd8cd15881c1ab2d308dcce
Levani's ART <levani.qotolashvili@gmail.com>
2020-04-10 01:49:33 +0400

    added react lazy to improve performance with code splitting

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -4,10 +4,12 @@ import Directory from '../../components/directory/directory.component';
 
 import { HomePageContainer } from './homepage.styles';
 
-const HomePage = () => (
-  <HomePageContainer>
-    <Directory />
-  </HomePageContainer>
-);
+const HomePage = () => {
+  return (
+    <HomePageContainer>
+      <Directory />
+    </HomePageContainer>
+  )
+};
 
 export default HomePage;
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import Spinner from '../../components/spinner/spinner.component';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
+const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
 
 class ShopPage extends React.Component {
   componentDidMount() {
@@ -17,14 +18,16 @@ class ShopPage extends React.Component {
     const { match } = this.props;
     return (
       <div className='shop-page'>
-        <Route 
-          exact path={`${match.path}`} 
-          component={CollectionsOverviewContainer}
-        />
-        <Route 
-          path={`${match.path}/:collectionId`} 
-          component={CollectionPageContainer}
-        />
+        <Suspense fallback={<Spinner />}>
+          <Route 
+            exact path={`${match.path}`} 
+            component={CollectionsOverviewContainer}
+          />
+          <Route 
+            path={`${match.path}/:collectionId`} 
+            component={CollectionPageContainer}
+          />
+        </Suspense>
       </div>
     );
   }
