ffe13c2a509c6ab5aa9417f2459b08077dc78215
Levani's ART <levani.qotolashvili@gmail.com>
2020-04-23 19:07:53 +0400

    Changed stripe checkout name

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -29,7 +29,7 @@ const StripeCheckoutButton = ({ price }) => {
   return (
     <StripeCheckout
       label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      name='Clothio Ltd.'
       billingAddress
       shippingAddress
       image='https://svgshare.com/i/CUz.svg'
