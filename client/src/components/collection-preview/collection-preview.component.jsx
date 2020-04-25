84f18d108ee5a0db43eabc48b0cf08521fa4d363
Levani's ART <levani.qotolashvili@gmail.com>
2020-04-26 02:44:51 +0400

    seperated mens and womens collection from other collections for home page

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -5,19 +5,21 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items, history, match, routeName }) => (
-  <div className="collection-preview d-flex flex-column">
-    <div className="title-container" onClick={() => history.push(`${match.path}/${routeName}`)}>
-      {title.toUpperCase()}
+const CollectionPreview = ({ title, items, history, match, routeName }) => {
+  return(
+    <div className="collection-preview d-flex flex-column">
+      <div className="title-container" onClick={() => history.push(`${match.path}/${routeName}`)}>
+        {title.toUpperCase()}
+      </div>
+      <div className="collection-preview-container">
+        {items
+          .filter((item, idx) => idx < 6)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
-    <div className="collection-preview-container">
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </div>
-  </div>
-);
+  )
+};
 
 export default withRouter(CollectionPreview);
--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -2,47 +2,78 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollectionsForPreview } from '../../redux/directory/directory.selectors';
+
 import ManImage1 from '../../assets/images/man1.png';
 import ManImage2 from '../../assets/images/man2.png';
 import WomanImage1 from '../../assets/images/woman1.png';
 import WomanImage2 from '../../assets/images/woman2.png';
 
-import MenuItem from '../menu-item/menu-item.component';
 import HeroSection from '../hero-section/hero-section.component';
 
 import './directory.styles.scss';
 
-const Directory = ({ sections }) => (
-  <div className="w-100">
-    <HeroSection 
-      image1={ManImage1}
-      image2={ManImage2}
-      titleSm="MEN’S NEW ARRIVALS"
-      titleLg={"THE LATEST \n AND GREATEST"}
-      shopLink="/shop/mens"
-    />
+import { fetchCollectionsStart } from '../../redux/directory/directory.actions';
 
-    <div className="container py-5">
-      <div className="directory">
-        {sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    </div>
+
+class Directory extends React.Component {
+  componentDidMount() {
+    const { fetchCollectionsStart } = this.props;
+    fetchCollectionsStart();
+  }
+  
+  render() {
+    const { collections } = this.props;
     
-    <HeroSection 
-      image1={WomanImage1}
-      image2={WomanImage2}
-      titleSm="WOMEN’S NEW ARRIVALS"
-      titleLg={"THE LATEST \n AND GREATEST"}
-      shopLink="/shop/womens"
-    />
-  </div>
-);
+    const mensCollection = collections.filter((item) => item.title === "Mens");
+    const womensCollection = collections.filter((item) => item.title === "Womens");
+    
+    return(
+      <div className="w-100">
+        <HeroSection 
+          image1={ManImage1}
+          image2={ManImage2}
+          titleSm="MEN’S NEW ARRIVALS"
+          titleLg={"THE LATEST \n AND GREATEST"}
+          shopLink="/shop/mens"
+        />
+
+        <div className="container py-5">
+          <div className="collection-overview container d-flex flex-column">
+            {mensCollection.map(({ id, ...otherCollectionProps }) => (
+              <CollectionPreview key={id} {...otherCollectionProps} />
+            ))}
+          </div>
+        </div>
+        
+        <HeroSection 
+          image1={WomanImage1}
+          image2={WomanImage2}
+          titleSm="WOMEN’S NEW ARRIVALS"
+          titleLg={"THE LATEST \n AND GREATEST"}
+          shopLink="/shop/womens"
+        />
+
+        <div className="container py-5">
+          <div className="collection-overview container d-flex flex-column">
+            {womensCollection.map(({ id, ...otherCollectionProps }) => (
+              <CollectionPreview key={id} {...otherCollectionProps} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySections
+  collections: selectCollectionsForPreview
 });
 
-export default connect(mapStateToProps)(Directory);
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Directory);
