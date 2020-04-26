e6d43bb58cda7881db1909cacc91499283af9b98
Levani's ART <levani.qotolashvili@gmail.com>
2020-04-27 02:42:48 +0400

    modified hero section to be responsive

--- client/src/components/hero-section/hero-section.component.jsx
+++ client/src/components/hero-section/hero-section.component.jsx
@@ -6,7 +6,7 @@ import './hero-section.styles.scss';
 const HeroSection = (props) => (
   <div className="hero-section mt-4">
     <div className="container d-flex align-items-center justify-content-between">
-      <div className="col-3 mt-n5 ml-n5 d-flex justify-content-end">
+      <div className="img-left col-3 mt-n5 ml-n5 d-flex justify-content-end">
         <img className="ml-n5 mt-n3" src={props.image1} alt=""/>
       </div>
       <div className="d-flex align-items-center flex-column">
@@ -14,7 +14,7 @@ const HeroSection = (props) => (
         <span className="title-lg text-center mt-2">{props.titleLg}</span>
         <Link to={props.shopLink} className="shop-button mt-3">SHOP NOW</Link>
       </div>
-      <div className="col-3 mt-n5 ml-n5">
+      <div className="img-right col-3 mt-n5 ml-n5">
         <img className="mr-n5 mt-n3 pl-4 ml-2" src={props.image2} alt=""/>
       </div>
     </div>
