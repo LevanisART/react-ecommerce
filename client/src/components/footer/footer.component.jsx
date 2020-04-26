9afe7fcd508a1b9fc262c34ae3f7f6e668ded1ef
Levani's ART <levani.qotolashvili@gmail.com>
2020-04-27 03:10:04 +0400

    changed hero section and footer styles to be responsive

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -5,11 +5,11 @@ import './footer.styles.scss';
 
 const Footer = () => (
   <footer className="footer py-5 mt-5">
-    <div className="container d-flex align-items-center justify-content-between flex-wrap">
-      <Link to="/">
+    <div className="container d-flex flex-column flex-sm-row align-items-center justify-content-between flex-wrap">
+      <Link className="mb-4 mb-sm-0" to="/">
         <Logo />
       </Link>
-      <span className="copyright">Copyright © 2020 Clothio, All Rights Reserved</span>
+      <span className="copyright mb-2 mb-sm-0 text-center text-sm-right">Copyright © 2020 Clothio, All Rights Reserved</span>
     </div>
   </footer>
 );
