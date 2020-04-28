989d75d45b91651f180f7d65d41a66dc810c2d70
Levani's ART <levani.qotolashvili@gmail.com>
2020-04-28 22:11:50 +0400

    added padding for sign in and sign up page

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -6,7 +6,7 @@ import SignUp from '../../components/sign-up/sign-up.component';
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-  <div className="container my-5">
+  <div className="container py-4 my-5">
     <div className="row d-flex justify-content-between">
       <SignIn />
       <SignUp />
